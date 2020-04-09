07d7299ac5d7a85aabfdf20b412bb756a2c8acc3
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-04-08 20:56:09 -0600

    info typed and submitted get pushed to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,60 @@
-// Web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyC0WJ6SC1RUZuykRbDXwCvKzlPX37ftOAw",
-    authDomain: "mcelis-f41a6.firebaseapp.com",
-    databaseURL: "https://mcelis-f41a6.firebaseio.com",
-    projectId: "mcelis-f41a6",
-    storageBucket: "mcelis-f41a6.appspot.com",
-    messagingSenderId: "972502596896",
-    appId: "1:972502596896:web:231935d681f58efee6eb45",
-    measurementId: "G-6CB769JM46"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+$(document).ready(function () {
 
-var database = firebase.database();
+    // Web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyC0WJ6SC1RUZuykRbDXwCvKzlPX37ftOAw",
+        authDomain: "mcelis-f41a6.firebaseapp.com",
+        databaseURL: "https://mcelis-f41a6.firebaseio.com",
+        projectId: "mcelis-f41a6",
+        storageBucket: "mcelis-f41a6.appspot.com",
+        messagingSenderId: "972502596896",
+        appId: "1:972502596896:web:231935d681f58efee6eb45",
+        measurementId: "G-6CB769JM46"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+    var database = firebase.database();
 
 
-$("button").on("click", function() {
-    var name = $("#trainName").val();
-    var destination = $("#destination").val();
-    var firstTrain = $("#trainTime").val();
-    var minutes = $("#minutes").val();
+    $("button").on("click", function () {
 
-    database.ref().push({
-        tName: name,
-        tDestination: destination,
-        initialT: firstTrain,
-        frequency: minutes
+		event.preventDefault();
+
+        var name = $("#trainName").val();
+        var destination = $("#destination").val();
+        var userTime = $("#trainTime").val();
+        var minutes = $("#minutes").val();
+
+        database.ref().push({
+            tName: name,
+            tDestination: destination,
+            initialT: userTime,
+            frequency: minutes
+        });
+
+        database.ref().on("child_added", function (snapshot) {
+            var companyName = snapshot.val().tName;
+            var dest = snapshot.val().tDestination;
+            var trainOne = snapshot.val().initialT;
+            var freqMin = snapshot.val().frequency;
+
+            var dateBegin = moment(trainOne, 'hh:mm A');
+            var dateNow = moment();
+
+
+
+            var newRow = $("<tbody>").append($("<tr>").append(
+                $("<td>").text(companyName),
+                $("<td>").text(dest),
+                $("<td>").text(freqMin),
+                $("<td>").text(nextArrival),
+                $("<td>").text(minAway)
+            ));
+            $("#schd").append(newRow);
+            moment(dateBegin).diff(moment(dateNow))
+        });
     });
 
-    console.log(tName);
-    console.log(tDestination);
-    console.log(initialT);
-    console.log(frequency);
-
-    database.ref().on("child_added", function (snapshot) {
-        var companyName = snapshot.val().tName;
-        var dest = snapshot.val().tDestination;
-        var trainOne = snapshot.val().initialT;
-        var freqMin = snapshot.val().frequency;
-
-        var dateBegin = moment(trainOne, 'hh:mm A');
-        var dateNow = moment();
-
-
-
-        var newRow = $("<tbody>").append($("<tr>").append(
-            $("<td>").text(companyName),
-            $("<td>").text(dest),
-            $("<td>").text(freqMin)
-           // $("<td>").text(nextArrival),
-           // $("<td>").text(minAway)
-        ));
-        $("#displayTable").append(newRow);
-        moment(dateBegin).diff(moment(dateNow),)
-    });
 });
